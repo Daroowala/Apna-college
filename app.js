084db81161ff45a97d5d68efe6077743041fc540
my name <blueshaperd3@gmail.com>
2024-07-03 01:10:57 +0500

    let

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 console.log("Apna Karachi");
 console.log("I luv javaScript");
 
-// variables
-fullName = "Rehan";
-age = 24;
+// variables can be declared and update. A globle scope variable
+var fullName = "Rehan";
+var age = 24;
 console.log(fullName)
 console.log(age)
+
+// let variale cannot be re-declared but can be update. A block scope variable
+let name =  "Rehan";
+let price = 1000;
+console.log(name);
+console.log(price)
