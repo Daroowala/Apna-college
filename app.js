78203bc381eee1a87202023264dd9a8ac6bcf608
my name <blueshaperd3@gmail.com>
2024-07-17 18:52:16 +0500

    Practice 2 for Lecture 4

--- app.js
+++ app.js
@@ -345,4 +345,21 @@
 // }
 
 // Practice 1 for Lecture 4
-let marks = [85, 97, 44, 37, 76, 60]
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// for (let val of marks) {
+//     sum += val;
+// }
+// let avg = sum / marks.length
+// console.log(`avg marks of class = ${avg}`);
+
+// Practice 2 for Lecture 4
+let items = [250, 645, 300, 900, 50];
+let i = 0;
+
+for (let val of items){
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+console.log(`value after offer = ${items[i]}`)
+i++;
+}
