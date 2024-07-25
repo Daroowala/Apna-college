a920b31d0c73b542e439848f6d49e0c26152fe69
my name <blueshaperd3@gmail.com>
2024-07-26 01:53:52 +0500

    Practice 4 for Lecture 5

--- app.js
+++ app.js
@@ -461,3 +461,25 @@
 //  let arrowMult = (x, y) => {
 //    return (x * y);
 //  }
+
+// Practice 4 for Lecture 5
+// function countVowels (str) {
+//     let count = 0;
+//     for (let char of str) {
+//         if (char === 'a'|| char === 'e'|| char === 'i'|| char === 'o'|| char === 'u') {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// Practice arrow function
+let countVow = (str) => {
+    let count = 0;
+    for (let char of str) {
+        if (char === 'a'|| char === 'e'|| char === 'i'|| char === 'o'|| char === 'u') {
+            count++;
+        }
+    }
+    return count;
+}
